bdc235a277f05ff1204e8523ad52bc081846b1b2
chenhaozhe <gaozhechen26@gmail.com>
2023-11-09 19:08:21 +0800

    origin edition

---
+++ video_master_backend/src/main/java/com/video_master/video_master_backend/VideoMasterBackendApplication.java
@@ -0,0 +1,13 @@
+package com.video_master.video_master_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class VideoMasterBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(VideoMasterBackendApplication.class, args);
+    }
+
+}
---
+++ video_master_backend/src/test/java/com/video_master/video_master_backend/VideoMasterBackendApplicationTests.java
@@ -0,0 +1,13 @@
+package com.video_master.video_master_backend;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class VideoMasterBackendApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
